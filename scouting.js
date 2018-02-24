ce3684f434dfd23ba47db7df0392bbe5bfa84e87
SomeoneSomebody <hverstrat@hotmail.com>
2018-02-24 10:57:28 -0500

    changed toggle color from 'cyan' to 'lightgrey'

--- scouting.js
+++ scouting.js
@@ -13,130 +13,130 @@ document.querySelectorAll('.toggledown').forEach(cell=>cell.addEventListener('cl
 
 
 document.getElementById("aleft").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("aleft").style.background = 'white';
   } else {
-    document.getElementById("aleft").style.background = 'cyan';
+    document.getElementById("aleft").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("aright").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("aright").style.background = 'white';
   } else {
-    document.getElementById("aright").style.background = 'cyan';
+    document.getElementById("aright").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("amiddle").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("amiddle").style.background = 'white';
   } else {
-    document.getElementById("amiddle").style.background = 'cyan';
+    document.getElementById("amiddle").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("midbar").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("midbar").style.background = 'white';
   } else {
-    document.getElementById("midbar").style.background = 'cyan';
+    document.getElementById("midbar").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("sidebar").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("sidebar").style.background = 'white';
   } else {
-    document.getElementById("sidebar").style.background = 'cyan';
+    document.getElementById("sidebar").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("otherrob").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("otherrob").style.background = 'white';
   } else {
-    document.getElementById("otherrob").style.background = 'cyan';
+    document.getElementById("otherrob").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("onerob").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("onerob").style.background = 'white';
   } else {
-    document.getElementById("onerob").style.background = 'cyan';
+    document.getElementById("onerob").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("tworob").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("tworob").style.background = 'white';
   } else {
-    document.getElementById("tworob").style.background = 'cyan';
+    document.getElementById("tworob").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("ownfault").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("ownfault").style.background = 'white';
   } else {
-    document.getElementById("ownfault").style.background = 'cyan';
+    document.getElementById("ownfault").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("otherfault").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("otherfault").style.background = 'white';
   } else {
-    document.getElementById("otherfault").style.background = 'cyan';
+    document.getElementById("otherfault").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("hasramp").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("hasramp").style.background = 'white';
   } else {
-    document.getElementById("hasramp").style.background = 'cyan';
+    document.getElementById("hasramp").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("canclimbramp").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("canclimbramp").style.background = 'white';
   } else {
-    document.getElementById("canclimbramp").style.background = 'cyan';
+    document.getElementById("canclimbramp").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("successonramp").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("successonramp").style.background = 'white';
   } else {
-    document.getElementById("successonramp").style.background = 'cyan';
+    document.getElementById("successonramp").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("yeslift").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("yeslift").style.background = 'white';
   } else {
-    document.getElementById("yeslift").style.background = 'cyan';
+    document.getElementById("yeslift").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("nolift").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("nolift").style.background = 'white';
   } else {
-    document.getElementById("nolift").style.background = 'cyan';
+    document.getElementById("nolift").style.background = 'lightgrey';
   };
 });
 
 document.getElementById("autoline").addEventListener('click', (event)=>{
-  if (event.target.style.background == 'cyan') {
+  if (event.target.style.background == 'lightgrey') {
       document.getElementById("autoline").style.background = 'white';
   } else {
-    document.getElementById("autoline").style.background = 'cyan';
+    document.getElementById("autoline").style.background = 'lightgrey';
   };
 });
 

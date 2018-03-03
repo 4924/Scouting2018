5ce0810a962f1074ccd01adda0f047586d8ac534
SomeoneSomebody <hverstrat@hotmail.com>
2018-03-03 10:21:14 -0500

    chart

--- Display/scouting.js
+++ Display/scouting.js
@@ -1,37 +1,65 @@
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'horizontalBar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Match 1", "Match 2"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [5, 2],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+                'rgba(55, 200, 100, 0.5)',
+                'rgba(55, 200, 100, 0.5)'
+            ]
+        }, 
+        {
+            data: [1, 6],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(54, 162, 235, 0.5)'
+            ]
         }]
     },
     options: {
         scales: {
+        	xAxes: [{
+        		stacked: true
+        	}],
             yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
+            	stacked: true
             }]
         }
     }
 });
+
+var ctx = document.getElementById("myChart2").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: ["Match 1", "Match 2"],
+        datasets: [{
+            data: [5, 2],
+            backgroundColor: [
+                'rgba(55, 200, 100, 0.5)',
+                'rgba(55, 200, 100, 0.5)'
+            ]
+        }, 
+        {
+            data: [1, 6],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(54, 162, 235, 0.5)'
+            ]
+        }]
+    },
+    options: {
+    	responsive: false,
+        scales: {
+        	xAxes: [{
+        		stacked: true
+        	}],
+            yAxes: [{
+            	stacked: true
+            }]
+        }
+    }
+});
+
